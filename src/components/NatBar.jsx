42147517aa172d69852335e2fc6d7632dcf8e193
sebaroi <sroizenberg@gmail.com>
2022-09-15 15:23:29 -0300

    agregando logo

--- src/components/NatBar.jsx
+++ src/components/NatBar.jsx
@@ -1,9 +1,16 @@
+import logoRash from '../imagenes/logoChico1.png';
+
 function NatBar (){
   return(
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"> Navbar</a>
+    <a className="navbar-brand" href="#"> 
+    <div className='logoRash'>
+        <img 
+        src={logoRash} 
+        alt='logo de Rash' />
+    </div> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
