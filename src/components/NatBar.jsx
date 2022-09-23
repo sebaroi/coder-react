b55ee766645d44bf687bc2d6a385c144af679913
sebasroi <sroizenberg@gmail.com>
2022-09-22 23:54:59 -0300

    Entrega

--- src/components/NatBar.jsx
+++ src/components/NatBar.jsx
@@ -4,7 +4,6 @@ import FormSearch from './FormSearch';
 import Secciones from './secciones';
 
 
-
 const NatBar = () => {
   return (
     <div>
@@ -17,14 +16,12 @@ const NatBar = () => {
         src={logoRash} 
         alt='logo de Rash' />
     </div> </a>
-{/*     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button> */}
     <div className="collapse navbar-collapse" id="navbarColor02">
       <ul className="navbar-nav me-auto">
 
         <Secciones />
       </ul>
+
     <FormSearch busqueda='Buscar Producto' TextoBoton='Buscar'/>
     </div>
   </div>
